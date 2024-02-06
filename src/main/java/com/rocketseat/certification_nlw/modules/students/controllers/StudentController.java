1558654d7328e85a697b1ef142a1ecb64512bf7c
Bruno H. Homem <brunohhomem@gmail.com>
2024-02-06 19:03:01 -0300

    Verify Certification

--- src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/StudentController.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/StudentController.java
@@ -1,19 +1,29 @@
 package com.rocketseat.certification_nlw.modules.students.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rocketseat.certification_nlw.modules.students.dto.VerifyIfHasCertificationDTO;
+import com.rocketseat.certification_nlw.modules.students.useCases.VerifyIfHasCertificationUseCase;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
 
+  @Autowired
+  private VerifyIfHasCertificationUseCase verifyIfHasCertificationUseCase;
+
   @PostMapping("/verifyIfHasCertification")
   public String verifyIfHasCertification(@RequestBody VerifyIfHasCertificationDTO verifyIfHasCertificationDTO) {
-    System.out.println(verifyIfHasCertificationDTO);
+
+    var result = this.verifyIfHasCertificationUseCase.execute(verifyIfHasCertificationDTO);
+    if (result) {
+      return "Usuário já fez a prova";
+
+    }
     return "Usuário pode fazer a prova.";
   }
 }
